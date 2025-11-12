d68305ecac87bc6de9aa04f8446d3fd05320a91d
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-11-12 15:45:13 +0500

    fix warning react17\18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App";
 
-import App from './App'
+import "./styles/index.css";
 
-import './styles/index.css'
+// Получаем корневой элемент
+const container = document.getElementById("root");
 
-const rootView = document.getElementById('root')
+// Создаём корень
+const root = createRoot(container);
 
-if (rootView) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootView
-  )
-}
+// Рендерим приложение
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
