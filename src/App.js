7842c6bc53148f1af93619433fdb086c08f1303f
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-11-12 16:41:48 +0500

    add data state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import {
+  getSizes,
+  getSize,
+  getProducts,
+  getProduct,
+  getProductColor,
+} from "./services/api";
+
 import Header from "./components/Header/Header.jsx";
 import Cart from "./pages/cart/cart.jsx";
 import Shop from "./pages/shop/shop.jsx";
 
 export default function App() {
+  const [productsState, setProductsState] = useState({
+    sizes: [],
+    products: [],
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dataProducts = await getProducts();
+        const dataSizes = await getSizes();
+        setProductsState((prevState) => {
+          return { ...prevState, products: dataProducts, sizes: dataSizes };
+        });
+      } catch (error) {
+        setProductsState((prevState) => {
+          return { ...prevState, error: error, loading: false };
+        });
+      } finally {
+        setProductsState((prevState) => {
+          return { ...prevState, loading: false };
+        });
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Router>
