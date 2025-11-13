9b2bf014200a359ce485b509d97515dfff5aeb40
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-11-13 11:43:41 +0500

    add cart count to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
-function Header() {
+function Header({ cartTotal }) {
   return (
     <div className={`${styles.header__block}`}>
       <div className={`${styles.header__links}`}>
         <Link to="/">Каталог</Link>
-        <Link to="cart">Корзина</Link>
+        <Link to="cart">Корзина: {cartTotal}</Link>
       </div>
     </div>
   );
